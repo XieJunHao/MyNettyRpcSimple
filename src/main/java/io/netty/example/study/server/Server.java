632ad33f95b73056c5c62a8ebc468bf31f2857f6
xiejh <xiejunhao@fsdcic.org>
2020-01-29 20:25:36 +0800

    试一下commit

--- src/main/java/io/netty/example/study/server/Server.java
+++ src/main/java/io/netty/example/study/server/Server.java
@@ -38,8 +38,6 @@ public class Server {
                 pipeline.addLast(new OrderProtocoEncoder());
                 pipeline.addLast(new LoggingHandler(LogLevel.INFO));
                 pipeline.addLast(new OrderServerProcessHandler());
-
-
             }
         });
 
